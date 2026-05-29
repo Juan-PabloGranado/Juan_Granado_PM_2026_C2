fec0bf1146d47159f1ecf94ea358e12c8487b223
Juan-PabloGranado <granadoduvaljuanpablo@gmail.com>
2026-05-29 02:32:29 -0400

    Programas asignados del capitulo 2
    Primeros 8 ejercicios del capítulo 2.

---
+++ Prog. 1 Cap. 2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    {
+    float PRO;
+    printf("\nIngrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+    {
+        printf("\nAprobado");
+    }
+    else (PRO <= 5);
+    {
+        printf("\nReprobado");
+    }
+
+    }
+}
